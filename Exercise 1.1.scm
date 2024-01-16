9272ee558d02f994d245cd6e6c8fa0bf6002dfec
Izen78 <83284377+Izen78@users.noreply.github.com>
2024-01-16 13:30:07 +1000

    Finished Exercise 1.8

--- Exercise 1.1.scm
+++ Exercise 1.1.scm
@@ -1,42 +1,40 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname |Exercise 1.1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#lang sicp
 ; Exercise 1.1
 
-10
+;10
 
-(+ 5 3 4)
+;(+ 5 3 4)
 
-(- 9 1)
+;(- 9 1)
 
-(/ 6 2)
+;(/ 6 2)
 
-(+ (* 2 4) (- 4 6))
+;(+ (* 2 4) (- 4 6))
 
-(define a 3)
+;(define a 3)
 
-(define b (+ a 1))
+;(define b (+ a 1))
 
-(+ a b (* a b))
+;(+ a b (* a b))
 
-(= a b)
+;(= a b)
 
-(if (and (> b a) (< b (* a b))) b a)
+;(if (and (> b a) (< b (* a b))) b a)
 
-(cond ((= a 4) 6)
-      ((= b 4) (+ 6 7 a))
-      (else 25))
+;(cond ((= a 4) 6)
+;      ((= b 4) (+ 6 7 a))
+;      (else 25))
 
-(+ 2 (if (> b a) b a))
+;(+ 2 (if (> b a) b a))
 
-(* (cond ((> a b) a)
-         ((< a b) b)
-         (else -1))
-   (+ a 1))
+;(* (cond ((> a b) a)
+;         ((< a b) b)
+;         (else -1))
+;   (+ a 1))
 
 ; Exercise 1.2
 
-(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
+;(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
 
 ; Exercise 1.3
 
@@ -49,9 +47,9 @@
           (sum-of-square x z) ; x and z
           (sum-of-square x y)))) ; x and y
 
-(two-largest 1 2 3)
-(two-largest 2 1 3)
-(two-largest 3 1 2)
+;(two-largest 1 2 3)
+;(two-largest 2 1 3)
+;(two-largest 3 1 2)
 
 ; Exercise 1.4
 
@@ -69,7 +67,7 @@
       0
       y))
 
-(test 0 (p))
+;(test 0 (p))
 
 ; Applicative Order:
 ; Infinite loop
@@ -78,3 +76,59 @@
 ; Returns 0
 
 ; Exercise 1.6
+
+; square root by Newton's Method
+(define (square x) (* x x))
+(define (average x y) (/ (+ x y) 2))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+(define (sqrt x) (sqrt-iter 1.0 x))
+
+(sqrt 9)
+(define (new-if predicate then-clause else-clause) ; Arguments: predicate, then-clause, else-clause
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter-if guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter-if (improve guess x)
+                     x)))
+(define (sqrt-if x) (sqrt-iter-if 1.0 x))
+
+; (sqrt-if 9)
+; Infinite Loop
+; Scheme uses applicative-order evaluation and since new-if is a procedure (NOT a special form) it evaluates all arguments before implementing the operator
+; The arguments are 'guess' and '(sqrt-iter-if (improve guess...' Thus, the latter will recurse infinitely.
+; ie. The predicate and two cases will evaluate no matter what the value of the predicate is
+
+; Exercise 1.7
+
+; Inaccurate answers since real numbers can't be represented fully using a finite number of bits
+
+; Wrong Answer
+;(sqrt_ 0.000000000123)
+;(square 0.0312500013107187390371485)
+
+; Doesn't finish 
+;(sqrt_ 123123123123123123.123)
+(define (good-enough?two guess x)
+  (< (/ (abs (- guess (improve guess x))) (guess)) 0.001))
+
+; Exercise 1.8
+(define (improve-cb y x) ; y is the guess
+  (/ (+ (/ x (square y)) (* 2 y)) 3))
+(define (cbrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cbrt-iter (improve-cb guess x) x)))
+
+(define (cbrt x) (cbrt-iter 1.0 x))

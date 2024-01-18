a1b0e4982c57330bc933f20d2298cd34fb34acb2
Izen78 <83284377+Izen78@users.noreply.github.com>
2024-01-18 16:37:28 +1000

    On Exercise 1.13

--- Exercise 1.1.scm
+++ Exercise 1.1.scm
@@ -92,7 +92,7 @@
                  x)))
 (define (sqrt x) (sqrt-iter 1.0 x))
 
-(sqrt 9)
+; (sqrt 9)
 (define (new-if predicate then-clause else-clause) ; Arguments: predicate, then-clause, else-clause
   (cond (predicate then-clause)
         (else else-clause)))
@@ -132,3 +132,66 @@
       (cbrt-iter (improve-cb guess x) x)))
 
 (define (cbrt x) (cbrt-iter 1.0 x))
+
+
+; Exercise 1.9
+; First procedure - linear recursive process
+;; (+ 4 5)
+;; (inc (+ 3 5))
+;; (inc (inc (+ 2 5)))
+;; (inc (inc (inc (+ 1 5))))
+;; (inc (inc (inc (inc (+ 0 5)))))
+;; (inc (inc (inc (inc 5))))
+;; (inc (inc (inc 6)))
+;; (inc (inc 7))
+;; (inc 8)
+;; 9
+; Second procedure - linear iterative process
+;; (+ 4 5)
+;; (+ 3 6)
+;; (+ 2 7)
+;; (+ 1 8)
+;; (+ 0 9)
+;; 9
+
+; Exercise 1.10
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(A 1 10) ; 1024
+(A 2 4) ; 65536
+(A 3 3) ; 65536
+
+(define (f n) (A 0 n)) ; computes 2n
+(define (g n) (A 1 n)) ; returns 2^n
+;(A 0 (A 1 (- n 1)))
+;(A 0 (A 0 ... n times (A 1 (1)))
+(define (h n) (A 2 3)) ; too long, answer is 2^(h(n-1))
+
+; Exercise 1.11
+; Recursive Process
+(define (f-rec n)
+  (if (< n 3)
+      n
+      (+ (f-rec (- n 1)) (* 2 (f-rec (- n 2))) (* 3 (f-rec (- n 3))))))
+; Iterative Process
+(define (f-iter a b c total) ; INCORRECT
+  (if (= total 0)
+      a
+      (f-iter (+ a (* 2 b) (* 3 c)) a b (- total 1))))
+(define (fx n)
+  (f-iter 2 1 0 n))
+
+; Exercise 1.12 ; INCORRECT
+(define (pascal-iter row col n)
+  (cond ((= row n) n)
+      ((or (= row 0) (= col row)) 1)
+      (else (+ (pascal-iter (- row 1) (- col 1) (+ n 1)) (pascal-iter (- row 1) col (+ n 1))))))
+
+; Exercise 1.13
+phi = (1 - root(5))/2
+Assume phi^n / root(5)
